a7542127f2e9e4871d9add0ac73811cffcde65b8
Tom Petrangelo <48896889+tpetrangelo@users.noreply.github.com>
2026-03-18 01:20:01 +0000

    inital commit, added base files and two post endpoints for fastapi

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env into environment
+
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
+AWS_S3_PLAYS_SOURCE = os.getenv("AWS_S3_PLAYS_SOURCE")
+AWS_S3_OPENWEATHER_SOURCE = os.getenv("AWS_S3_OPENWEATHER_SOURCE")
+
+if not AWS_S3_BUCKET:
+    raise RuntimeError("AWS_S3_BUCKET not set")
+
+if not AWS_S3_PLAYS_SOURCE:
+    raise RuntimeError("AWS_S3_PLAYS_SOURCE not set")
+
+if not AWS_S3_OPENWEATHER_SOURCE:
+    raise RuntimeError("AWS_S3_OPENWEATHER_SOURCE not set")
