2eb57a876801aac5280cf9b6fb54137bf367ab54
Tom Petrangelo <48896889+tpetrangelo@users.noreply.github.com>
2026-03-22 22:22:45 +0000

    updated deploy script to include dbt seed

--- deploy_airflow.sh
+++ deploy_airflow.sh
@@ -6,11 +6,21 @@ REPO_DIR=~/MusicMetrics
 
 echo "Pulling latest changes from GitHub..."
 cd $REPO_DIR
-git pull
+git fetch origin
+git reset --hard origin/main
+
+echo "Copying DAG to Airflow..."
+cp $REPO_DIR/music_metrics_pipeline/airflow/dags/music_pipeline.py $AIRFLOW_DIR/dags/
+
+echo "Installing dependencies..."
+pip install -r $REPO_DIR/docker/airflow/requirements.txt
+
+echo "Running dbt seed..."
+docker exec airflow_airflow-scheduler_1 /bin/bash -c "/home/airflow/.local/bin/dbt seed --project-dir /opt/airflow/MusicMetrics/music_metrics_pipeline/dbt/music_metrics --profiles-dir /opt/airflow/MusicMetrics/music_metrics_pipeline/dbt/music_metrics 2>&1"
 
 echo "Restarting Airflow..."
 cd $AIRFLOW_DIR
 docker-compose down
 docker-compose up -d
 
-echo "Done! Airflow is updated and running."
+echo "Done"
