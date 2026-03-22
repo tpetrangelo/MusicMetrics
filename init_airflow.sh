81ac64518465f5d64e06f2f367198d9e0b935d30
Tom Petrangelo <48896889+tpetrangelo@users.noreply.github.com>
2026-03-22 21:15:59 +0000

    updated some bash scripts

--- init_airflow.sh
+++ init_airflow.sh
@@ -2,11 +2,25 @@
 set -e
 
 AIRFLOW_DIR=~/airflow
+REPO_DIR=~/MusicMetrics
 
-echo "Initializing Airflow database..."
+echo "Pulling latest changes from GitHub..."
+cd $REPO_DIR
+git fetch origin
+git reset --hard origin/main
+
+echo "Copying DAG to Airflow..."
+cp $REPO_DIR/music_metrics_pipeline/airflow/dags/music_pipeline.py $AIRFLOW_DIR/dags/
+
+echo "Rebuilding Docker image..."
 cd $AIRFLOW_DIR
 docker-compose down
+docker-compose build --no-cache
+
+echo "Initializing Airflow..."
 docker-compose up airflow-init
+
+echo "Starting Airflow..."
 docker-compose up -d
 
-echo "Airflow initialized and running."
+echo "Done"
